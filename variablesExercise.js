a53871ee3e5493d460a6563077b280f45cc9a088
Grant Patterson <darkrider231@gmail.com>
2021-02-23 11:51:51 -0500

    VariablesExercise Completed

---
+++ variablesExercise.js
@@ -0,0 +1,41 @@
+// #1
+// Const
+
+// #2
+let num = 20;
+
+// #3
+num = 15;
+
+// #4
+num = num + 7;
+num += 7;
+
+// #5
+num = num - 1;
+num -= 1;
+
+// #6
+const num = "This is not a number";
+
+// #7
+let blank;
+
+// #8
+console.log(blank);
+
+// #9
+const blank = null;
+
+// BONUS
+// #1
+let quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01;
+
+// #2
+console.log(quarter, dime, nickel, penny);
+
+// #3
+let letters = "qwertyuiop";
+
+// #4
+console.log(letters[10].length);
